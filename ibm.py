9211400d3504f966b817cee0c41c9d67c98c3d4d
bansal51 <bansal51@purdue.edu>
2021-07-15 14:20:31 -0500

    Price prediction for 1 year in the future

---
+++ ibm.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from fbprophet import Prophet
+
+# load dataset from Yahoo Finance
+data = pd.read_csv("IBM.csv")
+print(data.head())
+
+# Printing out graph of IBM close prices by date
+close = data['Close']
+ax = close.plot(title = 'IBM')
+ax.set_xlabel('Date')
+ax.set_ylabel('Close')
+plt.show()
+
+# we only need the date and close tabs, creating a new dataframe 
+data["Date"] = pd.to_datetime(data["Date"], infer_datetime_format=True)
+data = data[["Date", "Close"]]
+
+# We are using the Facebook Prophet model --> need to rename columns
+data = data.rename(columns={"Date" : "ds", "Close" : "y"})
+
+# Predict Stock Trend of IBM using Facebook Prophet
+model = Prophet()
+model.fit(data)
+
+predict = model.make_future_dataframe(periods=365)
+forcast = model.predict(predict)
+print(forcast[["ds", "yhat", "yhat_lower", "yhat_upper"]].tail())
+
+# Graph the results
+graph = model.plot(forcast, xlabel="Date", ylabel="Price")
+plt.show()
