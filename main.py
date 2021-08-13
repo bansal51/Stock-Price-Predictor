c8df007772e3918c2cf4bf51c6653edec97cc209
bansal51 <bansal51@purdue.edu>
2021-08-13 13:05:19 -0500

    added general file for all stocks, input ticker

---
+++ main.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from fbprophet import Prophet
+import requests
+import time
+import datetime
+
+while (1):
+    today = datetime.datetime.today()
+    stock = input("What ticker would to like me to predict? ")
+    period1 = int(time.mktime(datetime.datetime(today.year - 1, today.month, today.day, 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(today.year, today.month, today.day, 23, 59).timetuple()))
+    interval = '1d' # 1d, 1m
+
+    csv_url = f'https://query1.finance.yahoo.com/v7/finance/download/{stock}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+
+    # load dataset from Yahoo Finance
+    data = pd.read_csv(csv_url)
+    print(data.head())
+
+    # Printing out graph of stock close prices by date
+    close = data['Close']
+    ax = close.plot(title = stock)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Close')
+    plt.show()
+
+    # we only need the date and close tabs, creating a new dataframe 
+    data["Date"] = pd.to_datetime(data["Date"], infer_datetime_format=True)
+    data = data[["Date", "Close"]]
+
+    # We are using the Facebook Prophet model --> need to rename columns
+    data = data.rename(columns={"Date" : "ds", "Close" : "y"})
+
+    # Predict Stock Trend of chosen stock using Facebook Prophet
+    model = Prophet()
+    model.fit(data)
+
+    predict = model.make_future_dataframe(periods=365)
+    forcast = model.predict(predict)
+    print(forcast[["ds", "yhat", "yhat_lower", "yhat_upper"]].tail())
+
+    # Graph the results
+    graph = model.plot(forcast, xlabel="Date", ylabel="Price")
+    plt.show()
+
+    start_or_end = input("Continue? Type yes or no: ")
+    if (start_or_end != 'yes'):
+        break
